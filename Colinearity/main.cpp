7905bf891c09f3d65b1db0519a03e109f9807f57
krzysztof-1 <krzysztofszczyrek1@gmail.com>
2024-09-16 08:18:32 +0200

    I wrote program in cpp that checks if 3 points are colinear

--- Colinearity/main.cpp
+++ Colinearity/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
-int main() {
-    auto result = (10 <=> 20) > 0;
-    std::cout << result << std::endl;
+bool areThreePointsColinear(float x1,float y1, float x2, float y2, float x3, float y3)
+{
+float a1,a2;
+if ((x1==x2)&&(x2==x3)||(y1==y2)&&(y2==y3)) 
+    return true;
+else 
+    {
+    a1=(y1-y2)/(x1-x2);
+    a2=(y3-y2)/(x3-x2);
+    return a1==a2;
+    }
+}
+
+int main()
+{
+float x1,x2,x3,y1,y2,y3;int numberOfTests;
+std::cin>>numberOfTests;
+while (numberOfTests--)
+{
+std::cin>>x1>>y1>>x2>>y2>>x3>>y3;
+if (areThreePointsColinear(x1,y1,x2,y2,x3,y3))
+std::cout<<"TAK"<<std::endl;
+else
+std::cout<<"NIE"<<std::endl;
+}
 }
---
+++ Colinearity/problem1.md
@@ -0,0 +1,9 @@
+# Problem: COLLINEARITY OF POINTS
+Task:
+Determine whether the given 3 points are collinear or not.
+
+## Input:
+First, the number of test cases t (1 < t ≤ 100) is given. In each subsequent line, the coordinates of three points are given as integers in the range [-1000, 1000]. The consecutive coordinates are separated by a tab.
+
+## Output:
+The output should be the word YES if the given three points are collinear, or the word NO if they are not collinear. Each answer is written on a separate line.
